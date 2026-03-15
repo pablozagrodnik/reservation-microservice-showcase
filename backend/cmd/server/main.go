9269a5c39f41c8b2c11a33c6aa52dde261060a5f
PabloZagrodnik <pawel.zagrodnik0@gmail.com>
2026-03-16 00:13:52 +0100

    Add deployment to DO workflow

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Running")
+		_, err := fmt.Fprint(w, "Running")
+		if err != nil {
+			return
+		}
 	})
 
 	fmt.Println(":8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
